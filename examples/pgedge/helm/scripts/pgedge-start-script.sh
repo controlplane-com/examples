1b6481a7c2508338e46b555d254196f45179eab0
igorchyts <>
2023-12-12 10:59:37 +0200

    online-boutique with pgEdge fixes

--- examples/pgedge/helm/scripts/pgedge-start-script.sh
+++ examples/pgedge/helm/scripts/pgedge-start-script.sh
@@ -39,13 +39,9 @@ if [ "`id -u`" = "0" ]; then
 
     cat <<EOF > /home/pgedge/pgedge.env
 HOSTNAME=$HOSTNAME
-
 CLUSTER_NODES="$CLUSTER_NODES"
-
 POSTGRES_DB=$POSTGRES_DB
-
 POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-
 EOF
 
     # and then rerun this script as pgedge
@@ -84,28 +80,28 @@ for NODE in $CLUSTER_NODES; do
     NODE_PORT="${NODE##*:}"
 
     if [ "$NODE_HOSTNAME" == "$HOSTNAME" ]; then
-    echo "This host ($HOSTNAME) is part of the cluster."
-    # sed -i 's/export PGPORT=5432/export PGPORT=$NODE_PORT/' pg16/pg16.env
-    # source pg16/pg16.env
-    SET_NAME="cpln_default"
-    output=$(./nodectl status pgedge)
-    pg_ctl_path="/opt/pgedge/pgedge/pg16/bin/pg_ctl"
+        echo "This host ($HOSTNAME) is part of the cluster."
+        # sed -i 's/export PGPORT=5432/export PGPORT=$NODE_PORT/' pg16/pg16.env
+        # source pg16/pg16.env
+        SET_NAME="cpln_default"
+        output=$(./nodectl status pgedge)
+        pg_ctl_path="/opt/pgedge/pgedge/pg16/bin/pg_ctl"
 
-    if ([ "$output" == "pgedge installed" ] || [ "$output" == "pgedge stopped" ]) && [ -f "$pg_ctl_path" ]; then
-        # Restore the password
-        cp /opt/pgedge/pgedge/pg16/.pgpass ~pgedge/.pgpass
-        ./nodectl start pg16
-        ./nodectl spock node-create $NODE_SHORT "host=$HOSTNAME port=$NODE_PORT user=pgedge dbname=$POSTGRES_DB" "${POSTGRES_DB}" || true
-        ./nodectl spock repset-create "${SET_NAME}" "${POSTGRES_DB}" || true
-        subscribe "$NODE_SHORT" "$CLUSTER_NODES" &
-    else
-        ./nodectl install pgedge -U $POSTGRES_DB -P $POSTGRES_PASSWORD -d $POSTGRES_DB -p $NODE_PORT
-        # backup the pass file
-        cp ~pgedge/.pgpass /opt/pgedge/pgedge/pg16/.pgpass
-        ./nodectl spock node-create $NODE_SHORT "host=$HOSTNAME port=$NODE_PORT user=pgedge dbname=$POSTGRES_DB" "${POSTGRES_DB}" || true
-        ./nodectl spock repset-create "${SET_NAME}" "${POSTGRES_DB}" || true
-        subscribe "$NODE_SHORT" "$CLUSTER_NODES" &
-    fi
+        if ([ "$output" == "pgedge installed" ] || [ "$output" == "pgedge stopped" ]) && [ -f "$pg_ctl_path" ]; then
+            # Restore the password
+            cp /opt/pgedge/pgedge/pg16/.pgpass ~pgedge/.pgpass
+            ./nodectl start pg16
+            ./nodectl spock node-create $NODE_SHORT "host=$HOSTNAME port=$NODE_PORT user=pgedge dbname=$POSTGRES_DB" "${POSTGRES_DB}" || true
+            ./nodectl spock repset-create "${SET_NAME}" "${POSTGRES_DB}" || true
+            subscribe "$NODE_SHORT" "$CLUSTER_NODES" &
+        else
+            ./nodectl install pgedge -U $POSTGRES_DB -P $POSTGRES_PASSWORD -d $POSTGRES_DB -p $NODE_PORT
+            # backup the pass file
+            cp ~pgedge/.pgpass /opt/pgedge/pgedge/pg16/.pgpass
+            ./nodectl spock node-create $NODE_SHORT "host=$HOSTNAME port=$NODE_PORT user=pgedge dbname=$POSTGRES_DB" "${POSTGRES_DB}" || true
+            ./nodectl spock repset-create "${SET_NAME}" "${POSTGRES_DB}" || true
+            subscribe "$NODE_SHORT" "$CLUSTER_NODES" &
+        fi
     break
     fi
 done
@@ -114,10 +110,8 @@ done
 wait
 
 
-/opt/pgedge/pgedge/pg16/bin/psql $POSTGRES_DB -c "SELECT * FROM spock.node;"
--p $NODE_PORT
+/opt/pgedge/pgedge/pg16/bin/psql $POSTGRES_DB -c "SELECT * FROM spock.node;" -p $NODE_PORT
 
-/opt/pgedge/pgedge/pg16/bin/psql $POSTGRES_DB -p $NODE_PORT -f
-/scripts/replication.sql
+/opt/pgedge/pgedge/pg16/bin/psql $POSTGRES_DB -p $NODE_PORT -f /scripts/replication.sql
 
 sleep 99999d
